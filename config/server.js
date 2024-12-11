e8b37dabe0cbf8588b9a6686f3b2718845d62ba5
Sai Nitipat <nitipatch@hotmail.com>
2024-12-11 11:47:48 +0700

    change port

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1337),
-  url: 'http://localhost:1337', 
+  port: env.int('PORT', 3000),
+  url: 'http://localhost:3000', 
        //'https://profound-kit-finer.ngrok-free.app',
   app: {
     keys: env.array('APP_KEYS'),
